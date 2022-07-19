f26d05adf5649fcc6d4ae685aa024256b24af968
6acks <icebiker99@mail.ru>
2022-07-19 19:22:56 +0700

    added first, second tests.

--- src/test/java/FirstSimpleTest.java
+++ src/test/java/FirstSimpleTest.java
@@ -1,6 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+@DisplayName("Первый тест")
 public class FirstSimpleTest {
-
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("@BeforeAll method!");
+    }
+    @DisplayName("Простой тест на assertEquals")
+    @Test
+    void firstTest(){
+        Assertions.assertEquals(1,1);
+    }
+    @DisplayName("Простой тест на assertTrue")
+    @Test
+    void secondTest(){
+        Assertions.assertTrue(7>6);
+    }
 
 }
